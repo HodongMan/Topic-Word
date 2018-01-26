e8fdf669522e6888e7f3db3c69bc2a748226fe60
hodongman <arshavin3@naver.com>
2018-01-27 02:29:13 +0900

    setting update

--- config/settings.py
+++ config/settings.py
@@ -79,10 +79,15 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'unithon6',
+        'USER' : 'root',
+        'PASSWORD' : '1234',
+        'HOST' : '127.0.0.1',
+        'PORT' : '3306'
     }
 }
+
 
 
 # Password validation
