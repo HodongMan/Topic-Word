1c53606b16bfea3cf555d0e92e737b6871ca443f
hodongman <arshavin3@naver.com>
2018-01-27 17:30:32 +0900

    default

--- board/models/board.py
+++ board/models/board.py
@@ -5,8 +5,8 @@ class Board(models.Model):
     user = models.CharField(max_length=200)
     title = models.CharField(max_length=200)
     content = models.TextField()
-    background_color = models.CharField(max_length=200)
-    color = models.CharField(max_length=200)
+    background_color = models.CharField(max_length=200, default="")
+    color = models.CharField(max_length=200, default="")
     
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now_add=True)
